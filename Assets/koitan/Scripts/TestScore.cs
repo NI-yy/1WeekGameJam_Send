66ac1998d7ca397accf709c53f562d7ff2bffdc0
KOITAN_DESKTOP\tk_ka <tk_kakutasu@yahoo.co.jp>
2023-12-22 23:22:44 +0900

    仮で各ステージのスコアを表示

--- Assets/koitan/Scripts/TestScore.cs
+++ Assets/koitan/Scripts/TestScore.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TestScore : MonoBehaviour
 {
     [SerializeField]
     int score = 100;
+    [SerializeField]
+    TextMeshProUGUI[] texts;
     // Start is called before the first frame update
     void Start()
     {
-        SaveManager.SaveAndSendScoreOnStage(0, score);
+        for (int i = 0; i < texts.Length; i++)
+        {
+            texts[i].text = $"{i + 1}\nScore : {SaveManager.GetScoreOnStage(i + 1)}";
+        }
     }
 
     // Update is called once per frame
